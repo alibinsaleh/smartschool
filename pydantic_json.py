851d35584aee67475e75668f5f07e348728efea9
Ali Almohammed Saleh <alibinsaleh@gmail.com>
2024-04-02 21:37:10 +0300

    Edited search_json.py

--- pydantic_json.py
+++ pydantic_json.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
-import requests
+"""Read students data from JSON file and search for a student by his ID"""
 import json
-from rich import print
+from rich import print as rich_print
 from pydantic import BaseModel
 
 
 class Student(BaseModel):
+    """Hold student personal data"""
     id: int
     name: str
     mobile: str
 
+
 def get_data():
+    """Get json data from file"""
     # Path to your JSON file (replace with the actual path)
     file_path = "students_data.json"
     # Open the file in read mode
     try:
-        with open(file_path, "r") as json_file:
-            # Load the JSON data into a Python object (usually a dictionary or list)
+        with open(file_path, "r", encoding="utf-8") as json_file:
+            # Load the JSON data into a Python object
+            # (usually a dictionary or list)
             data = json.load(json_file)
     except FileNotFoundError:
         print(f"Error: JSON file '{file_path}' not found.")
@@ -24,12 +28,14 @@ def get_data():
 
 
 def main():
+    """Make jazz noise here"""
     students = get_data()
     print(type(students))
     for student in students:
         s = Student(**student)
         # print(s) # print as json
-        print(s.model_dump()) # print as dictionary
-        
+        rich_print(s.model_dump())  # print as dictionary
+
+
 if __name__ == "__main__":
     main()
